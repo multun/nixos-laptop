5f9eb10c5baac2a215c7a43c7dd3e16297fc0c24
Victor "multun" Collod <victor.collod@epita.fr>
2022-06-01 17:16:22 +0200

    update for 21.11

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,9 @@
 
 { config, pkgs, ... }:
 
-{
+let
+  unstable = import <nixos-unstable> { };
+in {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
@@ -29,16 +31,44 @@
     };
 
     kernelPackages = pkgs.linuxPackages;
-    extraModulePackages = with config.boot.kernelPackages; [perf];
+    kernelModules = [ "acpi_call" ];
+    kernelParams = [
+      # # TODO: bench power consumption with and without this option
+      # "intel_pstate=disable"
+      # BEWARE: may cause lockups
+      "pcie_aspm=force"
+      # it's a laptop with a cpu from 2013, i'd rather use a vulnerable laptop than buy a new one
+      "mitigations=off"
+    ];
+    extraModulePackages = with config.boot.kernelPackages; [ perf acpi_call ];
     extraModprobeConfig = ''
+      # enable_dc=4 forces the GPU into lower power states
+      # enable_fbc=0 disables framebuffer compression
+      # mitigations=off disables mitigations for a data leak
+      options i915 modeset=1 enable_dc=4 enable_fbc=0 mitigations=off
+      # please disable if it isn't working well
+      options iwlwifi power_save=1 power_level=3 bt_coex_active=1
       options snd_hda_intel power_save=1
+    '';
+  };
+
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
     '';
   };
 
   powerManagement = {
     enable = true;
-    cpuFreqGovernor = "ondemand";
-    powertop.enable = true;
+    cpuFreqGovernor = "schedutil";
+  };
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      DEVICES_TO_DISABLE_ON_STARTUP = "wwan";
+    };
   };
 
   networking.hostName = "thinkingbus";
@@ -53,19 +83,16 @@
   hardware.acpilight.enable = true;
 
   # weird intel stuff
-#  nixpkgs.config.packageOverrides = pkgs: {
-#    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-#  };
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
 
   hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
     extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      libvdpau-va-gl
+      vaapiIntel
     ];
   };
 
@@ -106,7 +133,7 @@
 
   programs.dconf.enable = true;
 
-  # virtualisation.docker.enable = true;
+  virtualisation.docker.enable = true;
 
   # enable ssh-agent
   programs.ssh.startAgent = true;
@@ -117,7 +144,6 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
@@ -129,15 +155,45 @@
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
   # Enable sound using pipewire
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    package = unstable.pipewire;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-  };
+    media-session.config.bluez-monitor.rules = [
+      {
+        # Matches all cards
+        matches = [ { "device.name" = "~bluez_card.*"; } ];
+        actions = {
+          "update-props" = {
+            "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+            # mSBC is not expected to work on all headset + adapter combinations.
+            "bluez5.msbc-support" = true;
+            # SBC-XQ is not expected to work on all headset + adapter combinations.
+            "bluez5.sbc-xq-support" = true;
+          };
+        };
+      }
+      {
+        matches = [
+          # Matches all sources
+          { "node.name" = "~bluez_input.*"; }
+          # Matches all outputs
+          { "node.name" = "~bluez_output.*"; }
+        ];
+        actions = {
+          "node.pause-on-idle" = false;
+        };
+      }
+    ];
+};
 
   # Enable i3 Desktop Environment.
   services.xserver = {
@@ -192,6 +248,27 @@
     uid = 1000;
   };
 
+  # services.redis = {
+  #   enable = true;
+  # };
+  # services.postgresql = {
+  #   enable = true;
+  #   package = pkgs.postgresql_13;
+  #   extraPlugins = with pkgs.postgresql_13.pkgs; [ postgis ];
+  #   enableTCPIP = true;
+  #   authentication = pkgs.lib.mkOverride 10 ''
+  #       local   all    all                    trust
+  #       host    all    all    127.0.0.1/32    trust
+  #       host    all    all    172.17.0.0/16   trust
+  #       host    all    all    ::1/128         trust
+  #   '';
+  #   initialScript = pkgs.writeText "db-initscript" ''
+  #     CREATE ROLE multun WITH LOGIN PASSWORD 'nopasswd' CREATEDB;
+  #     CREATE DATABASE osrd;
+  #     GRANT ALL PRIVILEGES ON DATABASE osrd TO multun;
+  #   '';
+  # };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -199,5 +276,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "20.03"; # Did you read the comment?
-
 }
